9062f1eaf8a18ec8ce514a1df649262b5c5b4979
wangkuan <wangkuan.math@gmail.com>
2026-06-28 23:18:26 +0900

    添加图片API

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 BLOG_JSON = "https://whynotstudyf6.github.io/index.json"
+IMAGES_JSON = "https://whynotstudyf6.github.io/images.json"
 
 @app.get("/api/posts")
 def get_posts():
@@ -23,3 +24,13 @@ def get_post(title: str):
         if title in post["permalink"]:
             return post
     return {"error": "找不到文章"}
+
+@app.get("/api/images")
+def get_images():
+    data = requests.get(IMAGES_JSON).json()
+    return data
+
+@app.get("/api/images/random")
+def get_random_image():
+    data = requests.get(IMAGES_JSON).json()
+    return {"url": "https://whynotstudyf6.github.io" + random.choice(data)}
